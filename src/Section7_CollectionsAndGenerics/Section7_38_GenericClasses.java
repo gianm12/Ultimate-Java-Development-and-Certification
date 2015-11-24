01737e7df3a3281da37afcb381f77920042d2f74
gianm12 <gianmarroquin@yahoo.com>
2015-11-24 09:52:21 -0800

    Working on section 38

--- src/Section7_CollectionsAndGenerics/Section7_38_GenericClasses.java
+++ src/Section7_CollectionsAndGenerics/Section7_38_GenericClasses.java
@@ -2,13 +2,14 @@ package Section7_CollectionsAndGenerics;
 
 public class Section7_38_GenericClasses {
 public static void main(String[] args) {
-	System.out.println("hi");
+	
 	Section7_38_OracleArrayList list = new Section7_38_OracleArrayList();
 	String[] numbers = {"1", "2","3"};
-	//System.out.println("length" +numbers.length);
-			for (int i = 0; i < numbers.length; i++) {
+
+			for (int i = 0; i < 30; i++) {
 				
-				list.add(numbers[i]);
+				//list.add(numbers[i]);
+				list.add(Integer.toString(i));
 				
 			}
 			for (int i = 0; i < list.size(); i++) {
--- src/Section7_CollectionsAndGenerics/Section7_38_OracleArrayList.java
+++ src/Section7_CollectionsAndGenerics/Section7_38_OracleArrayList.java
@@ -1,17 +1,17 @@
 package Section7_CollectionsAndGenerics;
 
-public class Section7_38_OracleArrayList {
+public class Section7_38_OracleArrayList<T> {
 
-	private String[] mItems;
+	private T[] mItems;
 	private int mFilledSpots;
 	public Section7_38_OracleArrayList()
 	{
-		mItems = new String[10];
+		mItems = (T[]) new Object[10];
 		
 	}
 	public void add(String item)
 	{
-		System.out.println("im in add method! number is " + item);
+		//System.out.println("im in add method! number is " + item);
 		if(mFilledSpots< mItems.length)
 		{
 			mItems[mFilledSpots] = item;
@@ -19,8 +19,8 @@ public class Section7_38_OracleArrayList {
 		}
 		else
 		{
-			String[] copy = mItems;
-			mItems = new String [mItems.length +10];
+			T[] copy = mItems;
+			mItems =  (T[]) new Object[mItems.length +10];
 			for(int i = 0; i <copy.length; i++)
 			{
 				mItems[i] = copy[i];
@@ -31,8 +31,6 @@ public class Section7_38_OracleArrayList {
 	}
 	public String get(int index)
 	{
-		System.out.println("in the get method! index is: " + index);
-		
 		return mItems[index];
 	}
 	
